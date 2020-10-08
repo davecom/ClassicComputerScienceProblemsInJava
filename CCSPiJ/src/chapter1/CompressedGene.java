cef01efcfb2ffcaf6905ea39a373edb0c035ac6c
David Kopec <david@oaksnow.com>
2020-10-07 23:50:42 -0400

    fixed length

--- CCSPiJ/src/chapter1/CompressedGene.java
+++ CCSPiJ/src/chapter1/CompressedGene.java
@@ -64,7 +64,7 @@ public class CompressedGene {
 			return "";
 		}
 		// create a mutable place for characters with right capacity
-		StringBuilder builder = new StringBuilder(length / 2);
+		StringBuilder builder = new StringBuilder(length);
 		for (int i = 0; i < (length * 2); i += 2) {
 			final int firstBit = (bitSet.get(i) ? 1 : 0);
 			final int secondBit = (bitSet.get(i + 1) ? 1 : 0);
